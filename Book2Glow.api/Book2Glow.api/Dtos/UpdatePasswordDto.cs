df5477b1ede5c68a84e252ee12fc61fb9c5aa109
JulienPajot <julien.pajot@dupdevinci-edu.fr>
2025-05-19 17:49:41 +0200

    feat: add user update password

---
+++ Book2Glow.api/Book2Glow.api/Dtos/UpdatePasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Book2Glow.Api.Dtos
+{
+    public class UpdatePasswordDto
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
